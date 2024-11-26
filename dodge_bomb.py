1be014e6b33b67bb391e01040b77827d3576752a
kinbou <c0b221431e@edu.teu.ac.jp>
2024-11-26 15:31:10 +0900

    課題３：工科トンの向きを移動方向へ合わせる

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -5,7 +5,7 @@ import sys
 import pygame as pg
 
 
-WIDTH, HEIGHT = 1100, 650
+WIDTH, HEIGHT = 1100,650
 DELTA = {
     pg.K_UP: (0, -5),
     pg.K_DOWN: (0, +5),
@@ -16,11 +16,6 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
-    """
-    引数で与えられたRectが画面の中か外かを判定する
-    引数：こうかとんRect or 爆弾Rect
-    戻り値：真理値タプル（横，縦）／画面内：True，画面外：False
-    """
     yoko, tate = True, True
     if rct.left < 0 or WIDTH < rct.right:
         yoko = False
@@ -28,12 +23,35 @@ def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
         tate = False
     return yoko, tate
 
+def kokaton_rotate(kk_img: pg.Surface):  # 課題3:飛ぶ方向に従ってこうかとん画像を切り替える
+    """
+    こうかとんの移動量のタプルとrotozoomした画像の辞書を返す
+    引数:こうかとんの画像(Surface型)
+    戻り値:移動量タプルとrotozoom画像の辞書
+    """
+    kk_imgs = {}
+    kk_mv = [(0, +5), (+5, +5), (+5, 0), (+5, -5), (0, -5), (-5, -5), (-5, 0), (-5, +5), (0, 0)]  # 移動量のタプルのリスト
+    angle = -90  # こうかとんの角度
+    flip = True
+    for k in kk_mv[:8]:
+        if angle > 90:  # 角度が半周以上したら
+            angle -= 180  # 角度を一度リセットして
+            flip = False  # 左右反転
+        kk = kk_img
+        kk = pg.transform.flip(kk, flip, False)
+        kk = pg.transform.rotozoom(kk, angle, 2.0)
+        kk_imgs[k] = kk  # key:移動量タプル value:手を加えたこうかとんsurface
+        angle += 45  #角度を45度回す
+    kk_imgs[kk_mv[8]] = pg.transform.rotozoom(kk_img, 0, 2.0)
+    return kk_imgs
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
+    kk_imgs = kokaton_rotate(kk_img)  # 課題3:こうかとん画像を切り替えるための辞書を用意する関数
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
     bb_img = pg.Surface((20, 20))  # 爆弾用の空Surface
@@ -64,7 +82,7 @@ def main():
         # こうかとんが画面外なら，元の場所に戻す
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
-        screen.blit(kk_img, kk_rct)
+        screen.blit(kk_imgs[(sum_mv[0], sum_mv[1])], kk_rct)  # 課題3:kk_imgsがsum_mvにあうようにする
         bb_rct.move_ip(vx, vy)  # 爆弾動く
         yoko, tate = check_bound(bb_rct)
         if not yoko:  # 横にはみ出てる
